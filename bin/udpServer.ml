1cb039282cf5d98606d0defd8245f7004940559d
Aryan Godara <aryangodara03@gmail.com>
2023-08-10 16:31:09 +0530

    Update to using an enum for udp messages to simplify pattern matching
    Signed-off-by: Aryan Godara <aryangodara03@gmail.com>

--- bin/udpServer.ml
+++ bin/udpServer.ml
@@ -10,6 +10,7 @@ type pubsubmessage =
   | SubscribeRequest of string
   | UnsubscribeRequest of string
   | PublishRequest of string * string
+  | InvalidRequest of string
 (* 
 msg format:
 Subscribe <topic>
@@ -23,52 +24,32 @@ Deserialize the byte string into a format that can be sent to a midi device.
 
 let handle_message msg client_address =
   let lst = Str.split_delim (Str.regexp " ") msg in
-  match lst with
+  let udpMsg = match lst with
   | ["subscribe"; topic] -> SubscribeRequest (topic)
   | ["unsubscribe"; topic] -> UnsubscribeRequest (topic)
   | ["publish"; topic; message] -> PublishRequest (topic, message)
-  | _ -> failwith "Invalid pubsubmessage format"
-
-  (* match param_list with
-  | "Subscribe" :: topic :: _ -> 
-    TOPIC_FILTER.addSocket topic client_address;
-    (* print_endline ("All the clients subscribed to " ^ topic ^ " are: "); *)
-    (* let client_list = TOPIC_FILTER.getSockets topics; *)
-    (* List.iter (fun client -> print_endline (string_of_inet_addr client)) client_list; *)
-    "Subscribed to " ^ topic
-  | "Subscribe" :: _ -> "Invalid Message"
-  | "Unsubscribe" :: topic :: _ ->
-    TOPIC_FILTER.removeSocket topic client_address;
-    "Unsubscribed from " ^ topic
-  | "Unsubscribe" :: _ -> "Invalid Message"
-  | "Publish"  :: topic :: message :: [] ->
-    let msg_list = Str.split_delim (Str.regexp " ") message in
-      match msg_list with
-      | [ _; _; _ ] as msg_list ->
-          | "Publish" :: topic :: message :: [] ->
-            let msg_list = Str.split_delim (Str.regexp " ") message in
-            begin match msg_list with
-            | [ _; _; _ ] ->
-              let char_param_list = List.map string_to_char msg_list in
-              Play.play_note (Array.of_list char_param_list) ();
-              "Published on " ^ topic
-            | _ -> "Invalid message"
-            end
-          | "Publish" :: _ -> "Invalid message"
-              let msg_list = Str.split_delim (Str.regexp " ") message in
-              begin match msg_list with
-              | [ _; _; _ ] as msg_list ->
-                let char_param_list = List.map string_to_char msg_list in
-                Play.play_note (Array.of_list char_param_list) ();
-                "Published on " ^ topic
-              | _ -> "Invalid message"
-              end
-            | "Publish" :: _ -> "Invalid message"
-            | _ -> "Invalid message"
-      | _ -> ();
-    "Published on " ^ topic
-  | "Publish" :: [] -> "Invalid Message"
-  | _ -> "Invalid message" *)
+  | _ -> InvalidRequest ("Invalid pubsubmessage format")
+in
+  if udpMsg = InvalidRequest ("Invalid pubsubmessage format") then
+    "Invalid pubsubmessage format"
+  else
+    match udpMsg with
+    | SubscribeRequest (topic) -> 
+      TOPIC_FILTER.addSocket topic client_address;
+      "Subscribed to " ^ topic
+    | UnsubscribeRequest (topic) ->
+      TOPIC_FILTER.removeSocket topic client_address;
+      "Unsubscribed from " ^ topic
+    | PublishRequest (topic, message) ->
+      let msg_list = Str.split_delim (Str.regexp " ") message in
+      begin match msg_list with
+      | [ _; _; _ ] ->
+        let char_param_list = List.map string_to_char msg_list in
+        Play.play_note (Array.of_list char_param_list) ();
+        "Published on " ^ topic
+      | _ -> "Invalid message"
+      end
+    | InvalidRequest (msg) -> msg
 
 (* This function handle_request takes a server socket (server_socket)
    as input and recursively listens for incoming UDP packets from
